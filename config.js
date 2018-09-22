cc8690b6f138184bc02d9d3b693772a41e66fa19
Sadeesh Radhakrishnan <sadeesh.r@gmail.com>
2018-09-22 21:02:04 +0800

    some changes

--- config.js
+++ config.js
@@ -1,8 +1,12 @@
 module.exports = {
-    app: {
-  	port: 9898
-    },
-    db: {
-   	secret: 'db'
-    }
+  app: {
+    port: 9898
+  },
+  db: {
+    secret: 'Rz+fUULV9+6AhVd78puTTLJiSnVUJIt3d51qJukJ6Fp/q3D/FXAz4vDMzUhKIqc6k3F0MLBIjPV1gGPA'
+  },
+  token: {
+    provider: 'I5m7bS8+VfEmqlFv0YEdpx6I6SNl8lj6eE3BDxsmDQTWJFkmzVyqbfopV620v4NkCYdujmGlp12EH5qE',
+    client: 'IHHdEAkjg2NozFNsgLHJWhZFPW5K9jqaARWUB0y5jSw2d12dY4ysdLK5a5rnMQszMm2+kw5z1Lf+W4CB'
+  }
 };
--- server.js
+++ server.js
@@ -1,9 +1,55 @@
 const express = require('express')
-const app = express()
+const cors = require('cors')
 const redis = require('redis')
 const config = require('./config');
-const client = redis.createClient({password: config.db.password})
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const app = express()
+const rclient = redis.createClient({ password: config.db.secret })
 
-app.listen(config.app.port, () => console.log(`API Middleware app listening on port ${port}!`))
+app.use(express.json());
+app.use(cors())
+app.use(function (req, res, next) {
+    console.log(req.headers.authorization);
+    if (!req.headers.authorization) {
+        return res.status(403).json({ error: 'No Credentials' });
+    } else if (config.token.client !== req.headers.authorization) {
+        return res.status(403).json({ error: 'Invalid Authorization' });
+    }
+    next();
+});
+
+app.get('/', (req, res) => res.sendStatus(404))
+app.post('/', (req, res) => res.sendStatus(404))
+
+app.post('/_api/put/', function (req, res) {
+    console.log(req.body);
+    if (req.body && Object.keys(req.body).length !== 0) {
+        let data = req.body
+        if (data.mobileNum) {
+            rclient.hmset(data.mobileNum, data, function (err, reply) {
+                if (err)
+                    console.error(err)
+                else
+                    console.log(reply);
+                return res.sendStatus(200)
+            })
+        }
+    } else
+        return res.status(200).json({ error: 'EMPTY REQUEST' });
+});
+
+app.get('/_api/get/:phone', function (req, res) {
+    rclient.hgetall(req.params.phone, function (err, reply) {
+        if (err)
+            console.error(err);
+        else {
+            console.log(reply);
+            if (!reply)
+                return res.status(200).json({ mobileNum: req.params.phone, error: 'NO DATA FOUND' });
+            else
+                return res.status(200).json(reply);
+        }
+    });
+})
+
+app.listen(config.app.port, () => console.log(`API Middleware app listening on port ${config.app.port}!`))
