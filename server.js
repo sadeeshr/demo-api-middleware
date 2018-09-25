68b4f6604ae020a14610cb53812df43360b5f2a0
Sadeesh Radhakrishnan <sadeesh.r@gmail.com>
2018-09-25 20:25:23 +0800

    parsing the number added

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const redis = require('redis')
 const config = require('./config');
 const xmlparser = require('express-xml-bodyparser');
-const convert = require('xml-js');
+const util = require('util')
 
 const app = express()
 const rclient = redis.createClient({ password: config.db.secret })
@@ -24,11 +24,6 @@ app.get('/', (req, res) => res.sendStatus(404))
 app.post('/', (req, res) => res.sendStatus(404))
 
 app.post('/_api/put/', function (req, res) {
-    console.log(req.body);
-    let xml = req.body
-    var result1 = convert.xml2json(xml, { compact: true, spaces: 4 });
-    var result2 = convert.xml2json(xml, { compact: false, spaces: 4 });
-    console.log(result1, '\n', result2);
     // if (config.token.provider !== req.headers.authorization) {
     //     return res.status(403).json({ error: 'Invalid Authorization' });
     // }
@@ -36,9 +31,12 @@ app.post('/_api/put/', function (req, res) {
         return res.status(403).json({ error: 'Invalid IP Address' });
     }
     if (req.body && Object.keys(req.body).length !== 0) {
-        let data = req.body
-        if (data.singleNumber && data.singleNumber.number) {
-            rclient.hmset(data.singleNumber.number, data, function (err, reply) {
+        console.log(util.inspect(req.body, false, null, true /* enable colors */))
+        let singleNumber = req.body["soap:envelope"]["soap:body"]["ns2:broadcast"][0]["singlenumber"][0]["number"][0]
+        console.log("##### NUMBER ##### ", singleNumber)
+
+        if (singleNumber) {
+            rclient.hmset(singleNumber, req.body, function (err, reply) {
                 if (err)
                     console.error(err)
                 else
@@ -71,3 +69,52 @@ app.get('/_api/get/:phone', function (req, res) {
 })
 
 app.listen(config.app.port, () => console.log(`API Middleware app listening on port ${config.app.port}!`))
+
+// SAMPLE DATA
+
+// [{
+//         'ns2:broadcast':
+//             [{
+//                 '$': { 'xmlns:ns2': 'http://portability.teletech.si' },
+//                 messageheader:
+//                     [{
+//                         messageid: ['d41cfe89-6bd1-4c4d-be8c-048b5934f39b'],
+//                         messagename: ['Broadcast'],
+//                         messageversion: ['1'],
+//                         messagetype: ['Broadcast'],
+//                         senderid: ['CRDB'],
+//                         receiverid: ['9876'],
+//                         timestamp: ['2018-09-25T15:14:33.399+06:00'],
+//                         recipientno: ['57'],
+//                         recipientso: ['57'],
+//                         donorno: ['31'],
+//                         donorso: ['31']
+//                     }],
+//                 processtype: ['MOBILE'],
+//                 processname: ['All'],
+//                 porteddate: ['2018-09-25T15:14:33.400+06:00'],
+//                 singlenumber:
+//                     [{
+//                         number: ['8801380000057'],
+//                         recipientrc: ['57'],
+//                         donorrc: ['31'],
+//                         nrhrc: ['31'],
+//                         portedaction: ['INSERT']
+//                     }],
+//                 extension:
+//                     [{
+//                         '$': { encryptedKey: 'false', encryptedValue: 'false' },
+//                         key: ['preliminaryProcess'],
+//                         value:
+//                             [{
+//                                 _: 'Porting',
+//                                 '$':
+//                                 {
+//                                     'xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
+//                                     'xmlns:xs': 'http://www.w3.org/2001/XMLSchema',
+//                                     'xsi:type': 'xs:string'
+//                                 }
+//                             }]
+//                     }]
+//             }]
+//     }]
