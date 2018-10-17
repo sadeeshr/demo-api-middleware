bc3fe62a234f5d77f60237cded8fd454e514d8d2
Sadeesh Radhakrishnan <sadeesh.r@gmail.com>
2018-10-17 13:10:38 +0800

    updated changes

--- telnet.js
+++ telnet.js
@@ -5,28 +5,28 @@ const PORT = 8080
 let client = new net.Socket()
 
 function connect() {
-    console.log("[" + SERVER + ":" + PORT + "] " + "Telnet Connection STARTED")
+    console.log("[" + new Date().toISOString() + " : " + SERVER + ":" + PORT + "] " + "Telnet Connection STARTED")
     client.connect(
         PORT,
         SERVER,
         () => {
-            console.log("[" + SERVER + ":" + PORT + "] " + "Telnet CONNECTED")
+            console.log("[" + new Date().toISOString() + " : " + SERVER + ":" + PORT + "] " + "Telnet CONNECTED")
             client.end("INFIZILLION KEEP-ALIVE REQUEST")
             setTimeout(reconnect, 300000);
         }
     )
 
-    client.on("data", data => console.log("[" + SERVER + ":" + PORT + "] " + "Telnet DATA", " Received: " + data))
+    client.on("data", data => console.log("[" + new Date().toISOString() + " : " + SERVER + ":" + PORT + "] " + "Telnet DATA", " Received: " + data))
 
-    client.on("close", () => console.log("[" + SERVER + ":" + PORT + "] " + "Telnet Connection CLOSED"))
+    client.on("close", () => console.log("[" + new Date().toISOString() + " : " + SERVER + ":" + PORT + "] " + "Telnet Connection CLOSED"))
 
-    client.on("end", () => console.log("[" + SERVER + ":" + PORT + "] " + "Telnet Connection ENDED"))
+    client.on("end", () => console.log("[" + new Date().toISOString() + " : " + SERVER + ":" + PORT + "] " + "Telnet Connection ENDED"))
 
     client.on("error", console.error)
 }
 
 reconnect = () => {
-    console.log("[" + SERVER + ":" + PORT + "] " + "Telnet RECONNECT")
+    console.log("[" + new Date().toISOString() + " : " + SERVER + ":" + PORT + "] " + "Telnet RECONNECT")
     client.removeAllListeners()
     connect()
 }
